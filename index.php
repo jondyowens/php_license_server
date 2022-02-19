11f239059ec293d139212f814524f6b4a54008fb
jondy <jondyowens@gmail.com>
2022-02-19 12:43:56 -0800

    Adding starting page and manage licenses page with blank table.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+      
+<head>
+    <title>
+        Jondyprise License Manager
+    </title>
+</head>
+  
+<body style="text-align:center;">
+      
+    <h1 style="color:blue;">
+        Jondyprise
+    </h1>
+      
+    <h4>
+        Welcome to the Jondyprise License Manager
+    </h4>
+  
+    <form method="post">
+        <a href="manage_licenses.php" class="button">Manage Licenses</a>
+    </form>
+</head>
+  
+</html>
---
+++ manage_licenses.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<body style="text-align:center;">
+    <h1 style="color:blue;"> Manage Licenses </h1>
+    
+    <?php
+        echo "<table border='1' align='center'>
+        <tr>
+        <th>Key</th>
+        <th>Last Check-In</th>
+        <th># of Users</th>
+        <th>Remaining Users</th>
+        </tr>"
+    ?>
+</body>
+</html>
